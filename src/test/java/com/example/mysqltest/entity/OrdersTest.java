98cdc5195f77c3fb1877a0339b52ad814086ab2b
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-17 11:56:37 +0900

    test: queryDsl 테스트에 대한 코드 추가

--- src/test/java/com/example/mysqltest/entity/OrdersTest.java
+++ src/test/java/com/example/mysqltest/entity/OrdersTest.java
@@ -1,30 +1,43 @@
 package com.example.mysqltest.entity;
 
+import com.example.mysqltest.config.TestQueryDslConfig;
 import com.example.mysqltest.repository.OrdersRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 @TestPropertySource(properties = {
         "spring.jpa.hibernate.ddl-auto=validate"})
+@Import(TestQueryDslConfig.class)
+@Sql("classpath:createEntity.sql")
 class OrdersTest {
-
     @Autowired private EntityManager entityManager;
     @Autowired private OrdersRepository ordersRepository;
-
+    
+    @Test
+    
+    @DisplayName("queryDsl 테스트")
+    void queryDslTest() {
+        List<Orders> orders = ordersRepository.getOrdersList();
+        System.out.println(orders.size());
+    }
     @Test
     void emManagerTest() {
         User user = User.getInstance()
-                .setNickname("hello")
+                .setNickname("hell")
                 .setPassword("world").build();
 
         Orders orders = Orders.getInstance()
@@ -33,5 +46,9 @@ class OrdersTest {
                 .build();
 
         entityManager.persist(orders);
+        entityManager.flush();
     }
+    
+    
+    
 }
