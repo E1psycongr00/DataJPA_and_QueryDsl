39796383114a372079e983da1271a1e425b4f9b4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-17 11:48:59 +0900

    feat: queryDsl을 실제 테스트하기 위한 컨트롤러

---
+++ src/main/java/com/example/mysqltest/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.mysqltest.controller;
+
+import com.example.mysqltest.entity.Orders;
+import com.example.mysqltest.repository.OrdersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+    
+    private final OrdersRepository ordersRepository;
+    @GetMapping("/hello")
+    public List<Orders> getHello() {
+        return ordersRepository.getOrdersList();
+    }
+    
+    @PostMapping("/hello")
+    public Orders postHello(@RequestBody Orders orders) {
+        return ordersRepository.save(orders);
+    }
+}
