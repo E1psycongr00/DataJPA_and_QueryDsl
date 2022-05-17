aced3581747a046dc9e38623500b26656006cdb8
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-17 11:55:56 +0900

    fix: auto_increment된 id는 트랜잭션시 초기화되지 않기 때문에 의도적으로 초기화해야함
    - sql문 불러 올 때 auto_increment도 1부터 시작하도록 초기화

--- src/main/resources/createEntity.sql
+++ src/main/resources/createEntity.sql
@@ -1,13 +1,10 @@
-INSERT
-INTO
-  user
-  (nickname, password)
-VALUES
-  ('hello', '121');
+ALTER TABLE user auto_increment = 1;
+ALTER TABLE orders auto_increment = 1;
+INSERT INTO user (nickname, password) VALUES('hello', '121');
+INSERT INTO user (nickname, password) VALUES('hello1', '123');
+INSERT INTO user (nickname, password) VALUES('hello2', '124');
+INSERT INTO user (nickname, password) VALUES('hello3', '125');
+INSERT INTO user (nickname, password) VALUES('hello4', '126');
+INSERT INTO orders (item, user_user_id) VALUES ('hel1lo', 1);
+INSERT INTO orders (item, user_user_id) VALUES('korea', 3);
 
-INSERT
-INTO
-  orders
-  (item, user_user_id)
-VALUES
-  ('hello', 1);
